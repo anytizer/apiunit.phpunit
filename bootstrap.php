afed7be8df2b559538c966cfa9324b9bc3ccf55a
bimal <gist@bimal.org.np>
2017-12-10 06:20:35 -0700

    Cleanups

--- bootstrap.php
+++ bootstrap.php
@@ -2,35 +2,15 @@
 /**
  * Some API gateways need specific headers
  */
-$_SERVER["HTTP_USER_AGENT"] = "API Test Scripts";
+$_SERVER["HTTP_USER_AGENT"] = "API Unit Test";
 $_SERVER["REQUEST_URI"] = "";
 
 error_reporting(E_ALL|E_STRICT);
 
-require_once("classes/endpoints/class.settings.inc.php");
-require_once("classes/endpoints/class.endpoints.inc.php");
-require_once("classes/connections/class.relay.inc.php");
-require_once("classes/others/class.mimer.inc.php");
-require_once("classes/others/class.ReadyToUpload.inc.php");
+require_once("classes/backend\class.spl_include.inc.php");
+spl_autoload_register(array(new \backend\spl_include("./classes"), "namespaced_inc_dot"));
 
 $endpoints = new \endpoints\endpoints();
-#print_r($endpoints);
-#echo "DateTime API is: ", $endpoints->datetime;
-#die(" Done!");
-
-/**
- * Where is your API Server?
- */
-#define("API_URL", "http://api.example.com/index.php/api");
-define("API_URL", "http://localhost/");
-
-#file_put_contents(dirname(__FILE__)."/curl.log", "");
-
-/**
- * Only unit tests will set these values
- */
-$_POST = array();
-$_GET = array();
 
 if(!function_exists("curl_init"))
 {
